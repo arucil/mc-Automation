a24c34bbc4626612c8342ed4770b7ace9755786c
amlo <xplzjwz@gmail.com>
2017-03-18 08:42:00 +0800

    change version number

--- src/main/java/plodsoft/automation/Automation.java
+++ src/main/java/plodsoft/automation/Automation.java
@@ -15,7 +15,7 @@ import plodsoft.automation.blocks.ModBlocks;
       acceptedMinecraftVersions = "[1.10.2]")
 public class Automation {
    public static final String MODID = "automation";
-   public static final String VERSION = "1.0";
+   public static final String VERSION = "1.3";
    public static final String NAME = "Automation";
 
    @Mod.Instance(MODID)
